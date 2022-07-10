dcd08c486b9d3b95cf4a6b9208ae5a1ea64637f2
Nico993 <73552294+Nico993@users.noreply.github.com>
2022-07-10 16:29:08 -0300

    Delete CicucoloDeSangre directory

--- CicucoloDeSangre/categoria.py
+++
@@ -1,9 +0,0 @@
-class Categoria:
-    def __init__(self,Nombre, Cuota):
-        self.Nombre = Nombre
-        self.Cuota = Cuota
-
-
-#Inicializar los tipos de categoria
-Activo = Categoria("Activo",13250.45)
-Pasivo = Categoria("Pasivo",78740.25)
